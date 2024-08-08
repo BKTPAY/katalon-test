e3c2c6ff3609f50e0b6acc99e6d918f0c107491a
monurakbalfin <126054225+monurakbalfin@users.noreply.github.com>
2024-08-08 13:19:40 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,11 +4,11 @@
 $ANDROID_SDK_ROOT/emulator/emulator -avd test_avd  -use-system-libs -no-snapshot-load -no-snapshot-save -no-skin  -no-window -no-audio -no-boot-anim -gpu swiftshader_indirect &
 
 # Wait for the emulator to start
-adb wait-for-device
-adb shell input keyevent 82
+#adb wait-for-device
+#adb shell input keyevent 82
 
 # Optionally, run your tests here
 # ./gradlew connectedAndroidTest
 
 # Keep the container running
-tail -f /dev/null
+# tail -f /dev/null
