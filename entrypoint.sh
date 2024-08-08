26b07d198cd7ebf503d13c4ad4c24ba60389cb55
monurakbalfin <126054225+monurakbalfin@users.noreply.github.com>
2024-08-08 17:09:24 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,7 @@
  apt-get install -y libxkbcommon-x11-0
  apt-get install -y xorg
  apt-get install -y xvfb
-xhost +
-export DISPLAY=:0
+
 # Start the emulator in headless mode
 $ANDROID_SDK_ROOT/emulator/emulator -avd test_avd  -use-system-libs -no-snapshot-load -no-snapshot-save -no-skin  -no-window -no-audio -no-boot-anim -gpu swiftshader_indirect &
 
