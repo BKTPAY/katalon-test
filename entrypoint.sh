19c6cf674740990c3957bf17cade5bff7e77a2ce
monurakbalfin <126054225+monurakbalfin@users.noreply.github.com>
2024-08-08 11:56:57 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Start the emulator in headless mode
-$ANDROID_SDK_ROOT/emulator/emulator -avd test_avd  -use-system-libs   -no-window -no-audio -no-boot-anim -gpu swiftshader_indirect &
+$ANDROID_SDK_ROOT/emulator/emulator -avd test_avd  -use-system-libs -no-snapshot-load -no-snapshot-save -no-skin  -no-window -no-audio -no-boot-anim -gpu swiftshader_indirect &
 
 # Wait for the emulator to start
 adb wait-for-device
