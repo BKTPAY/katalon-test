e073a094f09abfc2397a3e18c2582213f7b739fd
monurakbalfin <126054225+monurakbalfin@users.noreply.github.com>
2024-08-09 17:02:22 +0300

    Update Script1721978914902.groovy

--- Scripts/Send_Money/Script1721978914902.groovy
+++ Scripts/Send_Money/Script1721978914902.groovy
@@ -28,7 +28,7 @@ WebUI.callTestCase(findTestCase('Login Success'), [:], FailureHandling.STOP_ON_F
 AndroidDriver<?> driver = MobileDriverFactory.getDriver()
 
 
-Mobile.startApplication(RunConfiguration.getProjectDir() +'/app-release (3).apk', true)
+Mobile.startApplication('app-release (3).apk', true)
 
 Mobile.tap(findTestObject('Object Repository/Send_Money/android.widget.TextView - Transfero'), 0)
 
