860eb9b1e07fba77eadd36b7b48ac8ee230e7f5d
monurakbalfin <126054225+monurakbalfin@users.noreply.github.com>
2024-08-10 14:13:02 +0300

    Update Script1721978914896.groovy

--- Scripts/Login Success/Script1721978914896.groovy
+++ Scripts/Login Success/Script1721978914896.groovy
@@ -18,7 +18,7 @@ import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
 
-Mobile.startApplication('app-release (3).apk', true)
+Mobile.startApplication('app-release (3).apk', false)
 
 WebUI.delay(200)
 
