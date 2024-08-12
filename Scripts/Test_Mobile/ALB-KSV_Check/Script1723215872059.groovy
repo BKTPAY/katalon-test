300d44f6a576d820bfa970c97ab12f858a0bc49f
monurakbalfin <126054225+monurakbalfin@users.noreply.github.com>
2024-08-12 14:32:24 +0300

    Update Script1723215872059.groovy

--- Scripts/Test_Mobile/ALB-KSV_Check/Script1723215872059.groovy
+++ Scripts/Test_Mobile/ALB-KSV_Check/Script1723215872059.groovy
@@ -17,7 +17,7 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
-def path = RunConfiguration.getProjectDir() + '/Data Files/app-release (4).apk'
+def path = RunConfiguration.getProjectDir() + '/Data Files/epara.apk'
 
 Mobile.startApplication(path, true)
 
