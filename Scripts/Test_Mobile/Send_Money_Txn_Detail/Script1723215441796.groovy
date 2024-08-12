88142cecea453e13dbe849350f51f44e5108ad5b
monurakbalfin <126054225+monurakbalfin@users.noreply.github.com>
2024-08-12 14:31:46 +0300

    Update Script1723215441796.groovy

--- Scripts/Test_Mobile/Send_Money_Txn_Detail/Script1723215441796.groovy
+++ Scripts/Test_Mobile/Send_Money_Txn_Detail/Script1723215441796.groovy
@@ -19,7 +19,7 @@ import org.openqa.selenium.Keys as Keys
 
 WebUI.callTestCase(findTestCase('Test_Mobile/Login Success'), [:], FailureHandling.STOP_ON_FAILURE)
 
-not_run: Mobile.startApplication('/Users/finartz/Downloads/app-release (3).apk', true)
+not_run: Mobile.startApplication('epara.apk', true)
 
 Mobile.tap(findTestObject('Object Repository/Send_Money_Txn_Detail/android.widget.TextView - Shih t gjitha'), 0)
 WebUI.delay(1)
