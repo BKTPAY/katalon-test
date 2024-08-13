57115d53218f656f29e70ef7c6353ce50a612cf4
monurakbalfin <126054225+monurakbalfin@users.noreply.github.com>
2024-08-13 11:20:55 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,7 @@
 
 
 # Start the emulator in headless mode
-$ANDROID_SDK_ROOT/emulator/emulator -avd test_avd  -use-system-libs -no-snapshot-load -no-snapshot-save -no-skin  -no-window -no-audio -no-boot-anim -no-boot-anim  > /tmp/emulator.log 2>&1 &
-tail -f /tmp/emulator.log &
+$ANDROID_SDK_ROOT/emulator/emulator -avd test_avd  -use-system-libs -no-snapshot-load -no-snapshot-save -no-skin  -no-window -no-audio -no-boot-anim -no-boot-anim -gpu off  &
 
 sleep 30
 export DISPLAY=:99
