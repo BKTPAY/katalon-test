ff4163578696b64a5b9b39034e5daabab6beddd3
monurakbalfin <126054225+monurakbalfin@users.noreply.github.com>
2024-08-13 10:55:59 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,11 +12,13 @@ export LIBGL_ALWAYS_INDIRECT=1
 export QT_X11_NO_MITSHM=1
 # Wait for the emulator to start
 adb wait-for-device
+
 boot_completed=false
 while [ "$boot_completed" != "1" ]; do
     boot_completed=$(adb shell getprop sys.boot_completed | tr -d '\r')
     sleep 2
 done
+
 adb shell input keyevent 82
 adb devices
 adb logcat -d > /tmp/emulator_logcat.txt
@@ -26,6 +28,7 @@ adb logcat -d > /tmp/emulator_logcat.txt
 pwd
 ls
 sleep 30
+adb shell getprop init.svc.bootanim
 
 
 katalonc.sh -projectPath=/test -noSplash -runMode=console  -retry=0 -testSuitePath="Test Suites/Test_Suite_Mobile" -browserType="Android" -deviceId="emulator-5554" -executionProfile="default" -apiKey="554d7f82-ac15-4b60-aab3-739c97658f70" --config -proxy.auth.option=NO_PROXY -proxy.system.option=NO_PROXY -proxy.system.applyToDesiredCapabilities=true  -webui.autoUpdateDrivers=true -appiumDirectory="/usr/local/lib/node_modules/appium"
