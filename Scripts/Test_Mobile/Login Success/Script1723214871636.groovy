9560b22ddc4e5e38432e0a04941ea2a6dd8f579f
monurakbalfin <126054225+monurakbalfin@users.noreply.github.com>
2024-09-04 12:05:00 +0300

    Update Script1723214871636.groovy

--- Scripts/Test_Mobile/Login Success/Script1723214871636.groovy
+++ Scripts/Test_Mobile/Login Success/Script1723214871636.groovy
@@ -39,30 +39,49 @@ Mobile.startApplication(path, true)
 Mobile.delay(22)
 
 Mobile.tap(findTestObject('Object Repository/ALB-KSV_Check/android.widget.TextView - Shqipria'), 0)
-
+Mobile.takeScreenshot();
 Mobile.delay(22)
+Mobile.takeScreenshot();
 
 Mobile.tap(findTestObject('Object Repository/ALB-KSV_Check/android.widget.TextView - Vazhdo'), 0)
+Mobile.takeScreenshot();
 
 Mobile.delay(22)
+Mobile.takeScreenshot();
 
 Mobile.tap(findTestObject('Object Repository/Login Success/android.widget.TextView - Vijo'), 0)
+Mobile.takeScreenshot();
 
 Mobile.delay(22)
+Mobile.takeScreenshot();
 
 Mobile.tap(findTestObject('Object Repository/Login Success/android.widget.TextView - Hyr'), 0)
+Mobile.takeScreenshot();
 
 Mobile.delay(33)
+Mobile.takeScreenshot();
 
 Mobile.tap(findTestObject('Object Repository/Login Success/android.widget.TextView - e-mail'), 0)
+Mobile.takeScreenshot();
+
 Mobile.delay(5)
+Mobile.takeScreenshot();
+
 Mobile.tap(findTestObject('Object Repository/Login Success/android.widget.TextView - e-mail'), 0)
+Mobile.takeScreenshot();
 
 Mobile.delay(33)
+Mobile.takeScreenshot();
+
 
 Mobile.tap(findTestObject('Object Repository/Login Success/android.widget.EditText - e-mail'), 0)
+Mobile.takeScreenshot();
+
 Mobile.delay(5)
+Mobile.takeScreenshot();
+
 Mobile.tap(findTestObject('Object Repository/Login Success/android.widget.EditText - e-mail'), 0)
+Mobile.takeScreenshot();
 
 
 Mobile.delay(23)
