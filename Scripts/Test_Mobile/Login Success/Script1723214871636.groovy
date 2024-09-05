836301ac829585f4fa6bf4a20e691105a6e0b474
monurakbalfin <126054225+monurakbalfin@users.noreply.github.com>
2024-09-05 16:30:48 +0300

    Update Script1723214871636.groovy

--- Scripts/Test_Mobile/Login Success/Script1723214871636.groovy
+++ Scripts/Test_Mobile/Login Success/Script1723214871636.groovy
@@ -123,16 +123,29 @@ Mobile.delay(22)
 
 AndroidDriver<?> driver = MobileDriverFactory.getDriver()
 
-Mobile.tap(findTestObject('Object Repository/Login Success/android.view.ViewGroup (1)'), 0)
-Mobile.tap(findTestObject('Object Repository/Login Success/android.view.ViewGroup (1)'), 0)
+Mobile.tap(findTestObject('Object Repository/Login Success/android.view.ViewGroup (1)'), 15)
+Mobile.delay(7)
+
+Mobile.tap(findTestObject('Object Repository/Login Success/android.view.ViewGroup (1)'), 15)
+Mobile.delay(7)
 
 driver.pressKey(new KeyEvent(AndroidKey.DIGIT_1))
+Mobile.delay(1)
+
 driver.pressKey(new KeyEvent(AndroidKey.DIGIT_2))
+Mobile.delay(1)
+
 driver.pressKey(new KeyEvent(AndroidKey.DIGIT_3))
+Mobile.delay(1)
+
 Mobile.takeScreenshot();
 
 driver.pressKey(new KeyEvent(AndroidKey.DIGIT_4))
+Mobile.delay(1)
+
 driver.pressKey(new KeyEvent(AndroidKey.DIGIT_5))
+Mobile.delay(1)
+
 driver.pressKey(new KeyEvent(AndroidKey.DIGIT_6))
 
 
