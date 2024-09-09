11e4226f7ccb7a1b6644eb7bb16a495bc0f919d8
monurakbalfin <126054225+monurakbalfin@users.noreply.github.com>
2024-09-09 14:26:28 +0300

    Update Script1723214871636.groovy

--- Scripts/Test_Mobile/Login Success/Script1723214871636.groovy
+++ Scripts/Test_Mobile/Login Success/Script1723214871636.groovy
@@ -103,7 +103,7 @@ AndroidDriver<?> driver = MobileDriverFactory.getDriver()
 
 Mobile.tap(findTestObject('Object Repository/Login Success/android.view.ViewGroup'), 15)
 Mobile.delay(22)
-Mobile.verifyElementVisible(findTestObject('Object Repository/Login Success/android.view.ViewGroup'),'123456', 15)
+Mobile.verifyElementVisible(findTestObject('Object Repository/Login Success/android.view.ViewGroup'), 15)
 Mobile.hideKeyboard()
 
 Mobile.setText(findTestObject('Object Repository/Login Success/android.view.ViewGroup'),'123456', 15)
